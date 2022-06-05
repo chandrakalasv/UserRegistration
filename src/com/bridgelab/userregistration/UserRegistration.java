93052797836358bd85bf3da43c835bf10062145d
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-05 15:00:05 +0530

    added code to validate user last name

--- src/com/bridgelab/userregistration/UserRegistration.java
+++ src/com/bridgelab/userregistration/UserRegistration.java
@@ -6,10 +6,15 @@ public class UserRegistration {
     public static boolean givenFirstName_WhenValid_ShouldReturnTrue(String validFirstName) {
         return Pattern.matches("^[A-Z][a-z]{2,}", validFirstName);
     }
+    public static boolean givenLastName_WhenValid_ShouldReturnTrue(String validLastName) {
+        return Pattern.matches("^[A-Z][a-z]{2,}", validLastName);
+    }
 
     public static void main(String[] args) {
-        boolean result = givenFirstName_WhenValid_ShouldReturnTrue("Ashaa");
-        System.out.println(result);
+        boolean firstName = givenFirstName_WhenValid_ShouldReturnTrue("Ashaa");
+        System.out.println("first name is valid");
+        boolean lastName = givenFirstName_WhenValid_ShouldReturnTrue("Ushhaa");
+        System.out.println("lastName is valid");
     }
 
 }
