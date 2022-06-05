13e2577fc5bb2d3b25bb4f6ad62ce99deba4f3fc
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-05 15:30:10 +0530

    added code to validate user password with minimum 8 characters

--- src/com/bridgelab/userregistration/UserRegistration.java
+++ src/com/bridgelab/userregistration/UserRegistration.java
@@ -15,16 +15,21 @@ public class UserRegistration {
     public static boolean givenUserMobileNum_WhenValidFormat_ShouldReturnTrue(String validUserMobileNum) {
         return Pattern.matches("^(91)[\\s][0-9]{10}", validUserMobileNum);
     }
+    public static boolean givenPassword_WhenValid_ShouldReturnTrue(String validUserPassword) {
+        return Pattern.matches("^(.*){8}", validUserPassword);
+    }
 
     public static void main(String[] args) {
         boolean firstName = givenFirstName_WhenValid_ShouldReturnTrue("Ashaa");
-        System.out.println("first name is valid");
+        System.out.println("first name is valid" + firstName);
         boolean lastName = givenFirstName_WhenValid_ShouldReturnTrue("Ushhaa");
-        System.out.println("lastName is valid");
+        System.out.println("lastName is valid" + lastName);
         boolean validUserMail = givenEmail_WhenValid_ShouldReturnTrue("abc.xyz@bl.co.in");
-        System.out.println("user mail id is valid");
+        System.out.println("user mail id is valid" + validUserMail);
         boolean validUserMobileNum = givenUserMobileNum_WhenValidFormat_ShouldReturnTrue("91 2345677892");
-        System.out.println("user mobile number is valid");
+        System.out.println("user mobile number is valid" + validUserMobileNum);
+        boolean validUserPassword = givenPassword_WhenValid_ShouldReturnTrue("wert67$%");
+        System.out.println("user password of minimum 8 char is valid" + validUserPassword);
 
     }
 
